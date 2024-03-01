dd356c268d853ccaaa6ae15ace7e9764bc859be9
MaximoFlores <floresmaximovalentin@gmail.com>
2024-03-01 08:26:08 -0300

    2do commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let numeroSecreto = generarNumeroSecreto();
-let intentos = 0;
+let intentos = 1;
 
 console.log(numeroSecreto);
 
@@ -12,12 +12,24 @@ function asignarTextoElemento(elemento, texto) {
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     
-    if (numeroDeUsuario === numeroSecreto) {
-        console.log('Acertaste el número!');
-    } 
+    if(numeroDeUsuario===numeroSecreto){
+        asignarTextoElemento("p",`Acertaste el número en ${intentos} ${intentos>1?"veces":"vez"}`);
+    }
+    else {
+    if(numeroDeUsuario>numeroSecreto){
+        asignarTextoElemento("p","El numero secreto es menor");
+    }
+    else{
+        asignarTextoElemento("p","El numero secreto es mayor");      
+        }
+    intentos++;
+    limpiarCaja();    
+    }
     return;
 }
-
+function limpiarCaja(){
+    document.getElementById("valorUsuario").value = "";
+}
 
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
